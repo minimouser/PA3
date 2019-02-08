dd2a85f3a8abfe0b82b4b3cf39efb292960cfd4d
minimouser <minimouser@gmail.com>
2019-02-07 22:46:41 -0800

    Finished project

--- PA 3/PA 3/main.c
+++ PA 3/PA 3/main.c
@@ -7,21 +7,19 @@
 
 #include "sources.h"
 
-double readGPA(FILE *infile);
-double readAge(FILE *infile);
-
 int main(void) {
 	//creates gpa and age variables
 	double gpa1 = 0.0, gpa2 = 0.0, gpa3 = 0.0, gpa4 = 0.0, gpa5 = 0.0;
 	double age1 = 0.0, age2 = 0.0, age3 = 0.0, age4 = 0.0, age5 = 0.0;
 	int stnd1 = 0, stnd2 = 0, stnd3 = 0, stnd4 = 0, stnd5 = 0;
 
+	//opens input and output file
 	FILE* infile = NULL;
 	FILE* outfile = NULL;
 	infile = fopen("input.dat", "r");
 	outfile = fopen("output.dat", "w");
 
-	//reads all the gpa and age info from the input.dat file
+	//reads all the gpa, age, and class standing info from the input.dat file
 	gpa1 = readGPA(infile);
 	stnd1 = readInt(infile);
 	age1 = readAge(infile);
@@ -42,20 +40,24 @@ int main(void) {
 	stnd5 = readInt(infile);
 	age5 = readAge(infile);
 
+	double gpaMean = 0.0;
+	//calculates mean of gpa and class standing and prints it
+	gpaMean = calcMeanPrint(outfile, gpa1, gpa2, gpa3, gpa4, gpa5);
+	calcMeanPrint(outfile, stnd1, stnd2, stnd3, stnd4, stnd5);
+	calcMeanPrint(outfile, age1, age2, age3, age4, age5);
 
+	//prints space
+	printSpace(outfile);
+
+	//calculates and prints gpa standard deviation
+	calcStaDevPrint(outfile, gpa1, gpa2, gpa3, gpa4, gpa5, gpaMean);
+
+	//calculates and prints min and max
+	calcMinMaxPrint(outfile, gpa1, gpa2, gpa3, gpa4, gpa5);
+
+	//closes files
 	fclose(infile);
 	fclose(outfile);
+
 	return 0; //indicates success
-}
-
-//reads GPA from file, returns GPA
-double readGPA(FILE *infile) {
-	readInt(infile);
-	return readDouble(infile);
-}
-
-//reads the age from file 
-double readAge(FILE *infile) {
-	return readDouble(infile);
-	readInt(infile);
 }
--- PA 3/PA 3/sources.c
+++ PA 3/PA 3/sources.c
@@ -4,14 +4,14 @@
 double readDouble(FILE *infile) {
 
 	double number = 0.0;
-	fscanf(infile, "%d", &number);
+	fscanf(infile, "%lf", &number);
 	return number;
 }
 
 //reads Integer from infile, returns integer from infile
 int readInt(FILE *infile) {
 
-	int number = 0.0;
+	int number = 0;
 	fscanf(infile, "%d", &number);
 	return number;
 }
@@ -64,20 +64,20 @@ double findMax(double number1, double number2, double number3, double number4, d
 	if (max < number2) {
 		max = number2;
 	}
-
+	
 	if (max < number3) {
 		max = number3;
 	}
-
+		
 	if (max < number4) {
 		max = number4;
 	}
-
+			
 	if (max < number5) {
 		max = number5;
 	}
 
-	return number5;
+	return max;
 }
 
 //finds minimum value, returns minimum value
@@ -106,5 +106,59 @@ double findMin(double number1, double number2, double number3, double number4, d
 
 //writes number to outfile
 void printDouble(FILE *outfile, double number) {
-	fscanf(outfile, "%d\n", number);
+	fprintf(outfile, "%.2lf\n", number);
+}
+
+//prints a blank line
+void printSpace(FILE *outfile) {
+	fprintf(outfile, "\n");
+}
+
+//calculates max and min and then prints the file to the outfile
+void calcMinMaxPrint(FILE *outfile, double value1, double value2, double value3, double value4, double value5) {
+	double min = 0.0, max = 0.0;
+
+	min = findMin(value1, value2, value3, value4, value5);
+	max = findMax(value1, value2, value3, value4, value5);
+	printDouble(outfile, min);
+	printDouble(outfile, max);
+}
+
+//calculates standard deviation and prints to the outfile
+void calcStaDevPrint(FILE *outfile, double value1, double value2, double value3, double value4, double value5, double mean) {
+	double deviation1 = 0.0, deviation2 = 0.0, deviation3 = 0.0, deviation4 = 0.0, deviation5 = 0.0;
+
+	deviation1 = calculateDevation(value1, mean);
+	deviation2 = calculateDevation(value2, mean);
+	deviation3 = calculateDevation(value3, mean);
+	deviation4 = calculateDevation(value4, mean);
+	deviation5 = calculateDevation(value5, mean);
+
+	double variance = 0.0, standardDeviation = 0.0;
+	variance = calculateVariance(deviation1, deviation2, deviation3, deviation4, deviation5, 5);
+	standardDeviation = calculateStandardDeviation(variance);
+
+	printDouble(outfile, standardDeviation);
+}
+
+//calculates mean of 5 values and prints it
+double calcMeanPrint(FILE *outfile, double value1, double value2, double value3, double value4, double value5) {
+	double sum = 0.0, mean = 0.0;
+	sum = calculateSum(value1, value2, value3, value4, value5);
+	mean = calculateMean(sum, 5);
+
+	printDouble(outfile, mean);
+	return mean;
+}
+
+//reads GPA from file, returns GPA
+double readGPA(FILE *infile) {
+	readInt(infile);
+	return readDouble(infile);
+}
+
+//reads the age from file 
+double readAge(FILE *infile) {
+	return readDouble(infile);
+	readInt(infile);
 }
